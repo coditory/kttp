bb2a043d8be10c4ff40c3b80c6e6a11acb725737
ogesaku <ogesaku@gmail.com>
2025-05-11 10:51:56 +0200

    Fix http method

--- kttp-api/src/main/kotlin/com/coditory/kttp/HttpRequestMethod.kt
+++ kttp-api/src/main/kotlin/com/coditory/kttp/HttpRequestMethod.kt
@@ -1,29 +1,66 @@
 package com.coditory.kttp
 
-enum class HttpRequestMethod : HttpSerializable {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    HEAD,
-    OPTIONS,
-    OTHER,
-    ;
+import java.util.concurrent.ConcurrentHashMap
 
+class HttpRequestMethod private constructor(
+    private val value: String,
+) : HttpSerializable {
+    override fun toString() = value
     override fun toHttpString(builder: Appendable) {
-        builder.append(this.name)
+        builder.append(value)
     }
 
     companion object {
+        private val otherCache = SmallCache<String, HttpRequestMethod>()
+        val GET = HttpRequestMethod("GET")
+        val POST = HttpRequestMethod("POST")
+        val PUT = HttpRequestMethod("PUT")
+        val DELETE = HttpRequestMethod("DELETE")
+        val HEAD = HttpRequestMethod("HEAD")
+        val OPTIONS = HttpRequestMethod("OPTIONS")
+
         fun parse(text: String): HttpRequestMethod {
-            return when (text.uppercase()) {
-                GET.name -> GET
-                POST.name -> POST
-                PUT.name -> PUT
-                DELETE.name -> DELETE
-                HEAD.name -> HEAD
-                OPTIONS.name -> OPTIONS
-                else -> OTHER
+            val uppercased = text.uppercase()
+            return when (uppercased) {
+                GET.value -> GET
+                POST.value -> POST
+                PUT.value -> PUT
+                DELETE.value -> DELETE
+                HEAD.value -> HEAD
+                OPTIONS.value -> OPTIONS
+                else -> otherCache.get(uppercased) { HttpRequestMethod(uppercased) }
+            }
+        }
+    }
+}
+
+private class SmallCache<K, V>(
+    private val maxSize: Int = 100,
+) {
+    private val cache = ConcurrentHashMap<K, V>()
+    private val ordered = mutableListOf<K>()
+
+    fun get(key: K): V? {
+        return cache[key]
+    }
+
+    fun get(key: K, provider: () -> V): V {
+        val value = cache[key]
+        if (value != null) {
+            return value
+        }
+        val created = provider()
+        put(key, created)
+        return created
+    }
+
+    fun put(key: K, value: V) {
+        val prev = cache.put(key, value)
+        if (prev == null) {
+            ordered.add(key)
+            if (cache.size > maxSize) {
+                val first = ordered.removeFirst()
+                cache.remove(first)
             }
         }
     }
